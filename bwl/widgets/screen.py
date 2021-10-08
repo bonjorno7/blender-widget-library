43ef27689f86929aee41ccbbb45ce4bb128a2031
bonjorno7 <jorijndegraaf@gmail.com>
2021-10-09 01:29:37 +0200

    Add comments to Screen
    Just little explanations of the code, hopefully not too useless.

--- bwl/widgets/screen.py
+++ bwl/widgets/screen.py
@@ -13,9 +13,11 @@ class Screen(Widget):
     def __init__(self):
         super().__init__()
 
+        # Don't render the screen itself.
         self.style.visibility = Visibility.HIDDEN
 
     def handle_event(self, state: ModalState, event: EventCopy) -> bool:
+        # Mouse clicks bring the clicked widget to the front.
         if event.press and event.type in ('LEFTMOUSE', 'RIGHTMOUSE', 'MIDDLEMOUSE', 'BUTTON4MOUSE', 'BUTTON5MOUSE'):
             for child in reversed(self.children):
                 if child.style.display != Display.NONE:
@@ -27,6 +29,7 @@ class Screen(Widget):
         return super().handle_event(state, event)
 
     def compute_layout(self, context: Context):
+        # Stretch screen to fill the 3D view.
         self.style.width = context.area.width - self.style.margin.width - self.style.border_thickness * 2
         self.style.height = context.area.height - self.style.margin.height - self.style.border_thickness * 2
 
