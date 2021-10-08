1171ceebe3728fcfc2e3dc7ca605d3cde373e4ff
JonasAlmaas <jonasalmaas7@gmail.com>
2021-10-08 22:41:57 +0200

    Fixed Screen event handling
    Made the event handling in Screen work as intended. If you are hovering a non focused window the events should not be passed through to the 3D viewport rather be treated as handled.

--- bwl/widgets/screen.py
+++ bwl/widgets/screen.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 from bpy.types import Context
 
-from ..input import ModalEvent, ModalState
+from ..input import EventCopy, ModalState
 from ..style import Display, Visibility
 from . import Widget
+
+# TODO: Allow scroll events on windows that are not focused.
 
 
 class Screen(Widget):
@@ -19,21 +21,31 @@ class Screen(Widget):
         self.style.height = context.area.height - self.style.margin.height - self.style.border_thickness * 2
         super().compute_layout(context)
 
-    def handle_event(self, state: ModalState, event: ModalEvent) -> bool:
+    def handle_event(self, state: ModalState, event: EventCopy) -> bool:
         children = [child for child in self.children if child.style.display != Display.NONE]
+
+        # You should not call this on a screen with no children.
         if not children:
             return False
 
-        # Mouse clicks focus the clicked window.
-        if event.type in ('LEFTMOUSE', 'RIGHTMOUSE') and event.value == 'PRESS':
+        # Mouse clicks focus the clicked widget.
+        if event.type in ('LEFTMOUSE', 'RIGHTMOUSE') and event.press:
             for child in reversed(children):
-                if child.layout.border.contains(state.mouse_x, state.mouse_y):
+                if child.is_mouse_inside(state):
                     self.children.remove(child)
                     self.children.append(child)
 
-                    # Mouse clicks inside a window are handled exclusively.
+                    # Mouse clicks inside a widget are handled exclusively.
                     child.handle_event(state, event)
                     return True
 
-        # Other events are handled by the focused window.
-        return children[-1].handle_event(state, event)
+        # Other events are handled by the focused widget.
+        if children[-1].handle_event(state, event):
+            return True
+
+        # If the cursor is on a widget, we always consider the event handled.
+        for child in reversed(children):
+            if child.is_mouse_inside(state):
+                return True
+
+        return False
