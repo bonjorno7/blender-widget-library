7ebc4632599ec84c461824c07398776449d35f8f
JonasAlmaas <jonasalmaas7@gmail.com>
2021-10-09 15:40:06 +0200

    Moved rendering code into functions
    Made three different sub functions for rendering a widget. render_standard, render_image and render_text. You can only call one of the two first ones, but render_text can be called reguardless of it being an image or not.

--- bwl/render.py
+++ bwl/render.py
@@ -9,7 +9,9 @@ from bpy.types import Context
 from gpu.types import GPUBatch, GPUShader
 from gpu_extras.batch import batch_for_shader
 
+from .content import Image
 from .layout import Area
+from .style import Color
 
 if TYPE_CHECKING:
     from .widgets import Widget
@@ -74,69 +76,126 @@ def render_widget(widget: Widget, context: Context) -> None:
         bgl.glScissor(scissor.x, context.area.height - scissor.y - scissor.height, scissor.width, scissor.height)
         bgl.glEnable(bgl.GL_SCISSOR_TEST)
 
+    bgl.glEnable(bgl.GL_BLEND)
+
     if widget.image is None:
-        if Shaders.standard is None:
-            raise Exception('Shader must be compiled first')
-
-        Shaders.standard.bind()
-        Shaders.standard.uniform_float('u_position', [x, y])
-        Shaders.standard.uniform_float('u_size', [width, height])
-        Shaders.standard.uniform_float('u_color', color)
-        Shaders.standard.uniform_float('u_border_color', border_color)
-        Shaders.standard.uniform_float('u_border_radius', border_radius)
-        Shaders.standard.uniform_float('u_border_thickness', border_thickness)
-
-        bgl.glEnable(bgl.GL_BLEND)
-        batch: GPUBatch = batch_for_shader(Shaders.standard, 'TRIS', {'position': vertices}, indices=indices)
-        batch.draw(Shaders.standard)
-        bgl.glDisable(bgl.GL_BLEND)
+        render_standard(
+            x=x,
+            y=y,
+            width=width,
+            height=height,
+            color=color,
+            border_color=border_color,
+            border_radius=border_radius,
+            border_thickness=border_thickness,
+            vertices=vertices,
+            indices=indices,
+        )
 
     else:
-        if Shaders.image is None:
-            raise Exception('Shader must be compiled first')
-
-        Shaders.image.bind()
-        Shaders.image.uniform_float('u_position', [x, y])
-        Shaders.image.uniform_float('u_size', [width, height])
-        Shaders.image.uniform_float('u_color', color)
-        Shaders.image.uniform_float('u_border_color', border_color)
-        Shaders.image.uniform_float('u_border_radius', border_radius)
-        Shaders.image.uniform_float('u_border_thickness', border_thickness)
-        Shaders.image.uniform_int('u_image', 0)
-
-        image = widget.image
-        if image.gl_load():
-            raise Exception('Failed to load image')
-
-        bgl.glEnable(bgl.GL_BLEND)
-        bgl.glActiveTexture(bgl.GL_TEXTURE0)
-        bgl.glBindTexture(bgl.GL_TEXTURE_2D, image.bindcode)
-        bgl.glTexParameteri(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MIN_FILTER, bgl.GL_LINEAR)
-        bgl.glTexParameteri(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MAG_FILTER, bgl.GL_LINEAR)
-
-        batch: GPUBatch = batch_for_shader(Shaders.image, 'TRIS', {'position': vertices}, indices=indices)
-        batch.draw(Shaders.image)
-
-        bgl.glBindTexture(bgl.GL_TEXTURE_2D, 0)
-        bgl.glDisable(bgl.GL_BLEND)
-
-        image.gl_free()
+        render_image(
+            image=widget.image,
+            x=x,
+            y=y,
+            width=width,
+            height=height,
+            color=color,
+            border_color=border_color,
+            border_radius=border_radius,
+            border_thickness=border_thickness,
+            vertices=vertices,
+            indices=indices,
+        )
 
     if widget.text is not None:
-        x = widget.layout.text.x
-        y = widget.layout.text.y
-        height = widget.layout.text.height
+        render_text(widget, context)
 
-        # Offset Y to work with OpenGL.
-        y = context.area.height - y - height
-
-        # Get the font ID once because it's a getter.
-        id = widget.text.style.font_id
-
-        blf.color(id, *widget.text.style.color)
-        blf.size(id, widget.text.style.font_size, 72)
-        blf.position(id, x, y, 0)
-        blf.draw(id, widget.text.data)
+    bgl.glDisable(bgl.GL_BLEND)
 
     if widget.layout.scissor != None:
         bgl.glDisable(bgl.GL_SCISSOR_TEST)
+
+
+def render_standard(
+    x: float,
+    y: float,
+    width: float,
+    height: float,
+    color: Color,
+    border_color: Color,
+    border_radius: float,
+    border_thickness: float,
+    vertices,
+    indices,
+):
+    if Shaders.standard is None:
+        raise Exception('Shader must be compiled first.')
+
+    Shaders.standard.bind()
+    Shaders.standard.uniform_float('u_position', [x, y])
+    Shaders.standard.uniform_float('u_size', [width, height])
+    Shaders.standard.uniform_float('u_color', color)
+    Shaders.standard.uniform_float('u_border_color', border_color)
+    Shaders.standard.uniform_float('u_border_radius', border_radius)
+    Shaders.standard.uniform_float('u_border_thickness', border_thickness)
+
+    batch: GPUBatch = batch_for_shader(Shaders.standard, 'TRIS', {'position': vertices}, indices=indices)
+    batch.draw(Shaders.standard)
+
+
+def render_image(
+    image: Image,
+    x: float,
+    y: float,
+    width: float,
+    height: float,
+    color: Color,
+    border_color: Color,
+    border_radius: float,
+    border_thickness: float,
+    vertices,
+    indices,
+):
+    if Shaders.image is None:
+        raise Exception('Shader must be compiled first.')
+
+    if image.gl_load():
+        raise Exception('Failed to load image.')
+
+    Shaders.image.bind()
+    Shaders.image.uniform_float('u_position', [x, y])
+    Shaders.image.uniform_float('u_size', [width, height])
+    Shaders.image.uniform_float('u_color', color)
+    Shaders.image.uniform_float('u_border_color', border_color)
+    Shaders.image.uniform_float('u_border_radius', border_radius)
+    Shaders.image.uniform_float('u_border_thickness', border_thickness)
+    Shaders.image.uniform_int('u_image', 0)
+
+    bgl.glActiveTexture(bgl.GL_TEXTURE0)
+    bgl.glBindTexture(bgl.GL_TEXTURE_2D, image.bindcode)
+    bgl.glTexParameteri(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MIN_FILTER, bgl.GL_LINEAR)
+    bgl.glTexParameteri(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MAG_FILTER, bgl.GL_LINEAR)
+
+    batch: GPUBatch = batch_for_shader(Shaders.image, 'TRIS', {'position': vertices}, indices=indices)
+    batch.draw(Shaders.image)
+
+    bgl.glBindTexture(bgl.GL_TEXTURE_2D, 0)
+
+    image.gl_free()
+
+
+def render_text(widget: Widget, context: Context):
+    x = widget.layout.text.x
+    y = widget.layout.text.y
+    height = widget.layout.text.height
+
+    # Offset Y to work with OpenGL.
+    y = context.area.height - y - height
+
+    # Get the font ID once because it's a getter.
+    id = widget.text.style.font_id
+
+    blf.color(id, *widget.text.style.color)
+    blf.size(id, widget.text.style.font_size, 72)
+    blf.position(id, x, y, 0)
+    blf.draw(id, widget.text.data)
