c2b5fbde44b177a8020080cc0eea5aa477b77861
bonjorno7 <jorijndegraaf@gmail.com>
2021-10-11 20:45:46 +0200

    Simplify example operator
    Just one window, in the center of the screen.
    Also got rid of imports in widgets init file, forgot to do it before.

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ from .bwl.input import ModalEvent, ModalState, Subscription
 from .bwl.render import compile_shaders
 from .bwl.style import Align, Color, Corners, Direction, Display, Sides, Size, Visibility
 from .bwl.utils import hide_hud, show_hud
-from .bwl.widgets import Button, Screen, Widget, Window
+from .bwl.widgets import Button, Widget
 
 
 class ExampleOperator(Operator):
@@ -44,59 +44,65 @@ class ExampleOperator(Operator):
                 res_font_roboto,
             )
 
-            # Setup screen.
-            self.screen = Screen()
-            self.screen.style.visibility = Visibility.VISIBLE
-            self.screen.style.padding = Sides(20)
-            self.screen.style.color = Color(0, 0.8)
+            # Setup root widget.
+            self.root = Widget()
+            self.root.style.visibility = Visibility.HIDDEN
+            self.root.style.width = Size.FLEX
+            self.root.style.height = Size.FLEX
+            self.root.style.align_x = Align.CENTER
+            self.root.style.align_y = Align.CENTER
 
             def escape_callback(state: ModalState) -> bool:
                 self.should_close = True
                 return True
 
-            self.screen.subscribe(
+            self.root.subscribe(
                 ModalEvent(type='ESC', value='PRESS'),
                 Subscription(escape_callback, reverse=True),
             )
 
             # Setup window A. Windows 11 themed.
-            window_a = Window(parent=self.screen)
-            window_a.style.direction = Direction.VERTICAL
-            window_a.style.x = 150
-            window_a.style.y = 150
-            window_a.style.width = 800
-            window_a.style.height = 600
-            window_a.style.color = Color(0.15)
-            window_a.style.border_color = window_a.style.color
-            window_a.style.border_radius = Corners(10)
-            window_a.style.border_thickness = 1
+            window = Widget(parent=self.root)
+            window.style.direction = Direction.VERTICAL
+            window.style.width = 800
+            window.style.height = 600
+            window.style.color = Color(0.15)
+            window.style.border_color = window.style.color
+            window.style.border_radius = Corners(10)
+            window.style.border_thickness = 1
+
+            # Consume all events where the cursor is on the window.
+            window.subscribe(ModalEvent(), Subscription(lambda state: True, area=True))
 
             # Setup title bar.
-            window_a.header.style.direction = Direction.HORIZONTAL
-            window_a.header.style.align_x = Align.CENTER
-            window_a.header.style.align_y = Align.CENTER
-            window_a.header.style.color = Color(0.2)
-            window_a.header.style.border_radius = Corners(9, 0)
-            window_a.header.text = Text('Blender Widget Library')
-            window_a.header.text.style.color = Color(0.8)
+            header = Widget(parent=window)
+            header.style.direction = Direction.HORIZONTAL
+            header.style.width = Size.FLEX
+            header.style.height = 32
+            header.style.align_x = Align.CENTER
+            header.style.align_y = Align.CENTER
+            header.style.color = Color(0.2)
+            header.style.border_radius = Corners(9, 0)
+            header.text = Text('Blender Widget Library')
+            header.text.style.color = Color(0.8)
 
             # Setup window icon.
-            image_blender = Widget(parent=window_a.header)
+            image_blender = Widget(parent=header)
             image_blender.style.width = Size.IMAGE
             image_blender.style.height = Size.IMAGE
             image_blender.style.margin = Sides(8)
             image_blender.image = res_image_blender
 
             # Title bar spacer.
-            spacer = Widget(parent=window_a.header)
+            spacer = Widget(parent=header)
             spacer.style.visibility = Visibility.HIDDEN
             spacer.style.width = Size.FLEX
 
             # Setup exit button.
-            exit_button = Button(window_a.header)
+            exit_button = Button(header)
             exit_button.style.width = 45
-            exit_button.style.height = window_a.header.style.height
-            exit_button.style.color = window_a.header.style.color
+            exit_button.style.height = header.style.height
+            exit_button.style.color = header.style.color
             exit_button.style.border_radius = Corners(0, 0, 9, 0)
             exit_button.style.align_x = Align.CENTER
             exit_button.style.align_y = Align.CENTER
@@ -118,9 +124,15 @@ class ExampleOperator(Operator):
             cross_icon.style.height = Size.IMAGE
             cross_icon.image = res_image_cross
 
-            window_a.frame.style.color = Color(0.25)
-            window_a.frame.style.padding = Sides(5)
-            window_a.frame.style.border_radius = Corners(0, 9)
+            # Setup content frame.
+            frame = Widget(parent=window)
+            frame.style.direction = Direction.VERTICAL
+            frame.style.width = Size.FLEX
+            frame.style.height = Size.FLEX
+            frame.style.color = Color(0.2)
+            frame.style.color = Color(0.25)
+            frame.style.padding = Sides(5)
+            frame.style.border_radius = Corners(0, 9)
 
             # Create scroll box widget type.
             class ScrollBox(Widget):
@@ -140,7 +152,7 @@ class ExampleOperator(Operator):
 
             # Setup scroll boxes.
             for direction in Direction:
-                scroll_box = ScrollBox(parent=window_a.frame)
+                scroll_box = ScrollBox(parent=frame)
                 scroll_box.style.display = Display.SCROLL
                 scroll_box.style.direction = direction
                 scroll_box.style.width = Size.FLEX if direction == Direction.HORIZONTAL else Size.AUTO
@@ -174,27 +186,8 @@ class ExampleOperator(Operator):
                     element.style.border_thickness = 1
                     element.text = Text('I am inside a scroll box')
 
-            # Setup window B.
-            window_b = Window(parent=self.screen)
-            window_b.style.x = 500
-            window_b.style.y = 500
-            window_b.style.width = 400
-            window_b.style.height = 600
-            window_b.style.color = Color(0.157, 0.173, 0.204)
-
-            # Setup title bar.
-            window_b.header.style.align_x = Align.CENTER
-            window_b.header.style.align_y = Align.CENTER
-            window_b.header.style.color.alpha = 0
-            window_b.header.text = Text('One Dark Pro')
-            window_b.header.text.style.color = Color(0.61)
-            window_b.header.text.style.font = res_font_roboto
-
-            # Setup frame.
-            window_b.frame.style.color = Color(0.129, 0.145, 0.169)
-
             # Finally compute the layout.
-            self.screen.compute_layout(self.state)
+            self.root.compute_layout(self.state)
             compile_shaders()
 
             self.setup(context)
@@ -207,14 +200,14 @@ class ExampleOperator(Operator):
     def modal(self, context: Context, event: Event) -> set:
         try:
             self.state = ModalState(context, event)
-            handled = self.screen.handle_event(self.state)
+            handled = self.root.handle_event(self.state)
 
             if self.should_close:
                 self.cleanup(context)
                 return {'FINISHED'}
 
             if handled:
-                self.screen.compute_layout(self.state)
+                self.root.compute_layout(self.state)
                 context.area.tag_redraw()
                 return {'RUNNING_MODAL'}
 
@@ -225,7 +218,7 @@ class ExampleOperator(Operator):
             raise
 
     def draw_callback(self):
-        self.screen.render(self.state)
+        self.root.render(self.state)
 
     def setup(self, context: Context) -> bool:
         hide_hud(context)
--- bwl/widgets/__init__.py
+++ bwl/widgets/__init__.py
@@ -1,4 +1,2 @@
 from .widget import Widget
-from .window import Window
-from .screen import Screen
 from .button import Button
